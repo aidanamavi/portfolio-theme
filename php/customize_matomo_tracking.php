d277a5b9513808c9e09ec04e1663f3b7eb7740fa
Aidan Amavi <mail@aidanamavi.com>
2021-12-21 20:43:26 -0500

    Moved Matomo function from theme footer to wp_footer hook.

--- php/customize_matomo_tracking.php
+++ php/customize_matomo_tracking.php
@@ -50,4 +50,31 @@ function customize_matomo_tracking( $wp_customize ) {
 }
 add_action( 'customize_register', 'customize_matomo_tracking' );
 
+function matomo_footer( ) {
+  $trackerUrl = get_theme_mod('matomo_tracker_url_textbox');
+  $trackerId = get_theme_mod('matomo_site_id_textbox');
+  if ($trackerUrl && $trackerId) :
+  ?><!-- Matomo -->
+            <script>
+            var _paq = _paq || [];
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+              var u="<?php echo $trackerUrl; ?>";
+              _paq.push(['setTrackerUrl', u+'matomo.php']);
+              _paq.push(['setSiteId', <?php echo $trackerId; ?>]);
+              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+              g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+            })();
+            </script>
+            <noscript>
+              <p>
+                <img src="<?php echo $trackerUrl; ?>matomo.php?idsite=<?php echo $trackerId; ?>&amp;rec=1" style="border:0;" alt="JavaScript required. Please enable." />
+              </p>
+            </noscript>
+            <!-- End Matomo Code --><?php
+  endif;
+}
+add_action( 'wp_footer', 'matomo_footer' );
+
  ?>
