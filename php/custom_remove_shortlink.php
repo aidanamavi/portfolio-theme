d3d8f1d3a87c4fb4967763d962611233329ca2a5
Aidan Amavi <mail@aidanamavi.com>
2022-02-27 15:46:18 -0500

    Created shortlink filter.

---
+++ php/custom_remove_shortlink.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* @package WordPress Portfolio Theme
+* @version 0.5
+* @author Aidan Amavi <mail@aidanamavi.com>
+* @link https://www.aidanamavi.com Author's Web Site
+* @copyright 2012 - 2021, Aidan Amavi
+* @license https://www.gnu.org/licenses/agpl.html GNU Affero General Public License
+*/
+
+add_filter('after_setup_theme', 'custom_remove_shortlink');
+function custom_remove_shortlink() {
+  remove_action('wp_head', 'wp_shortlink_wp_head', 10);
+  remove_action( 'template_redirect', 'wp_shortlink_header', 11);
+}
+
+?>
